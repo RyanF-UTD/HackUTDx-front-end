406b89ca18325ac042296cdd7a3e80823fe8cd6e
Courtney <courtneyldickenson@gmail.com>
2023-11-05 09:00:59 -0600

    landing page components

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,15 @@
 <template>
   <div class="q-pa-md">
-    <q-parallax :height= "700" :speed="0.5">
-      <template v-slot:media>
-        <img src="https://github.com/RyanF-UTD/HackUTDx-front-end/blob/Courtney/LandingPage/src/assets/elevate-nYgy58eb9aw-unsplash.jpg">
-      </template>
-
-      <h1 class="text-white">helloooo</h1>
-    </q-parallax>
+    <q-parallax src="src/assets/elevate-nYgy58eb9aw-unsplash.jpg"> </q-parallax>
   </div>
-
 
   <!--this is the slidy thingy with the tent-->
   <!--
 Forked from:
 https://quasar.dev/vue-components/carousel#example--transitions-bottom-navigation-arrows-and-auto-padding
 -->
-  <div id="q-app" style="min-height: 200vh">
+
+  <div id="q-app" style="min-height: 20vh">
     <div class="q-pa-md">
       <div class="q-gutter-md">
         <q-carousel
@@ -42,6 +36,8 @@ https://quasar.dev/vue-components/carousel#example--transitions-bottom-navigatio
             <div class="q-mt-md text-center">
               {{ lorem }}
             </div>
+
+            <!--place insides of carousel here-->
           </q-carousel-slide>
           <q-carousel-slide name="layers" class="column no-wrap flex-center">
             <q-icon name="layers" size="56px"></q-icon>
@@ -59,14 +55,67 @@ https://quasar.dev/vue-components/carousel#example--transitions-bottom-navigatio
       </div>
     </div>
   </div>
+
+  <div class="q-pa-md row items-center justify-center justify-between q-gutter-md">
+    <q-card class="my-card" flat bordered>
+      <q-card-section>
+        <div class="text-h6 q-mb-xs">Our Changing Planet</div>
+        <div class="row no-wrap items-center">
+          <q-rating size="18px" v-model="stars" :max="5" color="primary" />
+          <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
+        </div>
+      </q-card-section>
+
+      <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+    </q-card>
+    <q-card class="my-card" flat bordered>
+      <q-card-section>
+        <div class="text-h6 q-mb-xs">Our Changing Planet</div>
+        <div class="row no-wrap items-center">
+          <q-rating size="18px" v-model="stars" :max="5" color="primary" />
+          <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
+        </div>
+      </q-card-section>
+
+      <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+    </q-card>
+    <q-card class="my-card" flat bordered>
+      <q-card-section>
+        <div class="text-h6 q-mb-xs">Our Changing Planet</div>
+        <div class="row no-wrap items-center">
+          <q-rating size="18px" v-model="stars" :max="5" color="primary" />
+          <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
+        </div>
+      </q-card-section>
+
+      <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+    </q-card>
+  </div>
 </template>
 
 <!-- this is how to commment-->
 <script>
-import { defineComponent } from "vue";
+import { ref } from "vue";
 
-export default defineComponent({
-  name: "IndexPage",
-
-});
+export default {
+  setup() {
+    return {
+      slide: ref(1),
+      stars: ref(4),
+    };
+  },
+};
 </script>
+
+<style>
+.q-parallax-media {
+  height: 100vh;
+  min-height: 200x;
+  max-height: 400px;
+}
+
+.my-card {
+  width: 79%;
+  max-width: 300px;
+}
+</style>
